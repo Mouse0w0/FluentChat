86d2d9076fa4a023313b07c8b6212f3b0c5e8dd5
Mouse <mouse0w0@qq.com>
2019-05-31 00:16:07 +0800

    Update network

--- src/main/java/fluentchat/network/NetworkManager.java
+++ src/main/java/fluentchat/network/NetworkManager.java
@@ -2,6 +2,7 @@ package fluentchat.network;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class NetworkManager {
 
@@ -10,7 +11,14 @@ public class NetworkManager {
 
     private int nextId;
 
+    public <T extends Message> void register(Class<T> clazz, MessageSerializer<T> serializer) {
+        register(clazz, serializer, null);
+    }
+
     public <T extends Message> void register(Class<T> clazz, MessageSerializer<T> serializer, MessageHandler<T> handler) {
+        Objects.requireNonNull(clazz);
+        Objects.requireNonNull(serializer);
+
         int id = nextId++;
         RegisteredMessage<T> registeredMessage = new RegisteredMessage<>(id, clazz, serializer, handler);
         registeredMessages.put(clazz, registeredMessage);
